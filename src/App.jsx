d70369d74d38c932facb102c92a2d35aefe5db39
Lais Santos <lais.santos.cris05@gmail.com>
2025-12-01 10:24:57 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ function App() {
     <>
       <h1 className={s.title}>Rick and Morty Characters</h1>
       <main>
-        <input type="text" value={searchPage} onChange={(e) => searchPage(e.target.value)} placeholder="1/42" />
-        <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder="Procure um personagem"
-        
-        />
+        <div style={{display:"flex",flexWrap:"wrap", gap:"20px", alignItens:"center", justifyContent:"center"}}>
+          <input type="text" value={searchPage} onChange={(e) => setSearchPage(e.target.value)} placeholder="1/42" />
+          <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder="Procure um personagem"/>
+        </div>
 
         <div className={s.wrapCards}>
           {data.map((item, index) => {
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -13,10 +13,10 @@ import s from './card.module.css'
 
 export const Card = ({image, name, species}) => {
     return(
-        <>
-        <h2>{name}</h2>
-        <img src={image} alt={name} />
-         <p>{species}</p>
-        </>
+        <div className={s.cardBackground}>
+        <img className={s.cardImage} src={image} alt={name} />
+        <h2 className={s.carTitle}>{name}</h2>
+         <p className={s.cardText}>{species}</p>
+        </div>
     )
 }
